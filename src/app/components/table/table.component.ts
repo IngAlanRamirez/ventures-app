79d81c690fa49c95b2f7aa28d5229a177cfef457
Alan Christian Ramírez Rodríguez <ingenieroacramirez@gmail.com>
2025-07-02 16:28:35 -0600

    Agrega la tabla de características con soporte para vinculación de tarjeta y requisitos de tarjeta. Mejora la estructura del componente y los estilos correspondientes.

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -1,14 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { addIcons } from 'ionicons';
+import { checkmarkCircleOutline } from 'ionicons/icons';
+import { IonIcon } from '@ionic/angular/standalone';
+
+interface FeatureRow {
+  feature: string;
+  noCardRequired: boolean;
+  cardLinked: boolean;
+}
+
+const FEATURES: FeatureRow[] = [
+  { feature: 'Instant Coupons', noCardRequired: true, cardLinked: true },
+  {
+    feature: 'Full access to Visa Savings Edge benefits',
+    noCardRequired: false,
+    cardLinked: true,
+  },
+  { feature: 'Cashback tracking', noCardRequired: false, cardLinked: true },
+  {
+    feature: 'Merchant location search',
+    noCardRequired: false,
+    cardLinked: true,
+  },
+  { feature: 'Cashback offers', noCardRequired: false, cardLinked: true },
+];
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.scss'],
+  imports: [IonIcon, MatTableModule],
 })
-export class TableComponent  implements OnInit {
+export class TableComponent implements OnInit {
+  displayedColumns: string[] = ['feature', 'noCardRequired', 'cardLinked'];
+  dataSource = new MatTableDataSource(FEATURES);
 
-  constructor() { }
+  constructor() {
+    addIcons({ checkmarkCircleOutline });
+  }
 
   ngOnInit() {}
-
 }
