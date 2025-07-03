06e92451018c82bc16d5abf7c253c691840517bd
Alan Christian Ramírez Rodríguez <ingenieroacramirez@gmail.com>
2025-07-02 21:08:13 -0600

    Refactor el componente de pie de página para utilizar MatSelect en lugar de un elemento select estándar. Agrega módulos de Angular Material necesarios para mejorar la selección de idioma.

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,14 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { IonIcon } from '@ionic/angular/standalone';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatOptionModule } from '@angular/material/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
-  imports: [IonIcon],
+  imports: [IonIcon, MatFormFieldModule, MatSelectModule, MatOptionModule],
   standalone: true,
 })
 export class FooterComponent implements OnInit {
+  selectedLang = 'en';
+
   constructor() {}
 
   ngOnInit() {}
