214cb411c2cef6edace0850c858f59e50f5a8666
Alan Christian Ramírez Rodríguez <ingenieroacramirez@gmail.com>
2025-07-02 21:20:33 -0600

    Actualiza el ícono de idioma en el pie de página y agrega la importación de iconos para mejorar la selección de idioma.

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -3,18 +3,23 @@ import { IonIcon } from '@ionic/angular/standalone';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 import { MatOptionModule } from '@angular/material/core';
+import { languageOutline } from 'ionicons/icons';
+import { addIcons } from 'ionicons';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
   imports: [IonIcon, MatFormFieldModule, MatSelectModule, MatOptionModule],
-  standalone: true,
 })
 export class FooterComponent implements OnInit {
   selectedLang = 'en';
 
-  constructor() {}
+  constructor() {
+    addIcons({
+      languageOutline,
+    });
+  }
 
   ngOnInit() {}
 }
